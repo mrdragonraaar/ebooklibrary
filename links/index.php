92dd1da09364bcfbcec35940ca19832960457644
mrdragonraaar <ade@mrdragonraaar.com>
2013-10-29 04:34:45 +0000

    Updated links page to use dynamic data.

--- links/index.php
+++ links/index.php
@@ -7,87 +7,30 @@
 define('ICON_PAGE', '/global/icons/fugue-icons/icons/chain.png');
 include_once(__DIR__ . '/../header.php');
 ?>
-<!-- Links -->
+<!-- Links Page -->
 <div id="links_page">
 
-<!-- Amazon -->
+<?php foreach (get_links() as $link) { ?>
+<!-- <?php echo $link->title; ?> -->
 <div class="link_section">
-<h2 class="link_title social amazon">Amazon</h2>
+<h2 class="link_title"><img class="link_icon" src="<?php echo $link->icon; ?>" /><?php echo $link->title; ?></h2>
+<?php
+foreach ($link->urls() as $label => $url)
+{
+?>
 <div class="link_entry">
-<span class="link_label">Amazon UK</span>
-<a target="_blank" href="http://amazon.co.uk/">(amazon.co.uk)</a>
+<span class="link_label"><?php echo $label; ?></span>
+<a target="_blank" href="<?php echo $url; ?>">(<?php echo url_display($url); ?>)</a>
 </div>
-<div class="link_entry">
-<span class="link_label">Amazon US</span>
-<a target="_blank" href="http://amazon.com/">(amazon.com)</a>
+<?php
+}
+?>
 </div>
-</div>
-<!-- END Amazon -->
+<!-- END <?php echo $link->title; ?> -->
 
-<!-- FreeBookSpot -->
-<div class="link_section">
-<h2 class="link_title social freebookspot">FreeBookSpot</h2>
-<div class="link_entry">
-<span class="link_label">FreeBookSpot</span>
-<a target="_blank" href="http://www.freebookspot.es/">(freebookspot.es)</a>
+<?php } ?>
 </div>
-</div>
-<!-- END FreeBookSpot -->
-
-<!-- Goodreads -->
-<div class="link_section">
-<h2 class="link_title social goodreads">Goodreads</h2>
-<div class="link_entry">
-<span class="link_label">Goodreads</span>
-<a target="_blank" href="http://www.goodreads.com/">(goodreads.com)</a>
-</div>
-</div>
-<!-- END Goodreads -->
-
-<!-- Library Genesis -->
-<div class="link_section">
-<h2 class="link_title social libgen">Library Genesis</h2>
-<div class="link_entry">
-<span class="link_label">Library Genesis</span>
-<a target="_blank" href="http://gen.lib.rus.ec/">(gen.lib.rus.ec)</a>
-</div>
-</div>
-<!-- END Library Genesis -->
-
-<!-- Project Gutenberg -->
-<div class="link_section">
-<h2 class="link_title social gutenberg">Project Gutenberg</h2>
-<div class="link_entry">
-<span class="link_label">Project Gutenberg</span>
-<a target="_blank" href="http://gutenberg.org/">(gutenberg.org)</a>
-</div>
-<div class="link_entry">
-<span class="link_label">Project Gutenberg of Australia</span>
-<a target="_blank" href="http://gutenberg.net.au/">(gutenberg.net.au)</a>
-</div>
-<div class="link_entry">
-<span class="link_label">Project Gutenberg of Canada</span>
-<a target="_blank" href="http://www.gutenberg.ca/">(gutenberg.ca)</a>
-</div>
-<div class="link_entry">
-<span class="link_label">Project Gutenberg DE</span>
-<a target="_blank" href="http://gutenberg.spiegel.de/">(gutenberg.spiegel.de)</a>
-</div>
-</div>
-<!-- END Project Gutenberg -->
-
-<!-- Smashwords -->
-<div class="link_section">
-<h2 class="link_title social smashwords">Smashwords</h2>
-<div class="link_entry">
-<span class="link_label">Smashwords</span>
-<a target="_blank" href="http://www.smashwords.com/">(smashwords.com)</a>
-</div>
-</div>
-<!-- END Smashwords -->
-
-</div>
-<!-- END Links -->
+<!-- END Links Page -->
 <?php
 include_once(__DIR__ . '/../footer.php');
 ?>
